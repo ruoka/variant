7957596222f21463f17edf7bc694d437f9c87f07
ruoka <ruoka@users.noreply.github.com>
2016-11-17 02:02:50 +0200

    assignables

---
+++ test/std/utilities/variant/variant.variant/variant.assign/copy.pass.cpp
@@ -0,0 +1,25 @@
+#include <variant>
+#include <cassert>
+
+struct foo
+{
+    foo& operator = (const foo&) = default;
+};
+
+struct bar
+{
+    bar& operator = (const bar&) = delete;
+};
+
+int main()
+{
+    static_assert(std::is_copy_assignable_v<std::variant<foo>>);
+    static_assert(std::is_copy_assignable_v<std::variant<int,bool,double,foo>>);
+    static_assert(std::is_copy_assignable_v<std::variant<foo,int,bool,double>>);
+    static_assert(std::is_copy_assignable_v<std::variant<int,foo,bool,double>>);
+
+    static_assert(!std::is_copy_assignable_v<std::variant<bar>>);
+    static_assert(!std::is_copy_assignable_v<std::variant<int,bool,double,bar>>);
+    static_assert(!std::is_copy_assignable_v<std::variant<bar,int,bool,double>>);
+    static_assert(!std::is_copy_assignable_v<std::variant<int,bool,bar,double>>);
+}
