689ecaefcfdd3ff808a9a1c7a922accf29abefcd
ruoka <ruoka@users.noreply.github.com>
2016-11-19 15:36:11 +0200

    more ctor tests

--- test/std/utilities/variant/variant.variant/variant.ctor/default.pass.cpp
+++ test/std/utilities/variant/variant.variant/variant.ctor/default.pass.cpp
@@ -3,20 +3,50 @@
 
 struct foo
 {
-    foo() = default;
+    foo() noexcept = default;
 };
 
 struct bar
 {
     bar() = delete;
+    ~bar() = default;
 };
 
-int main()
+void test_type_traits()
 {
     static_assert(std::is_default_constructible_v<std::variant<foo>>);
     static_assert(std::is_default_constructible_v<std::variant<int,bool,double,foo>>);
     static_assert(std::is_default_constructible_v<std::variant<foo,int,bool,double>>);
+
+    static_assert(std::is_nothrow_default_constructible_v<std::variant<foo>>);
+    static_assert(std::is_nothrow_default_constructible_v<std::variant<int,bool,double,foo>>);
+    static_assert(std::is_nothrow_default_constructible_v<std::variant<foo,int,bool,double>>);
+
     static_assert(!std::is_default_constructible_v<std::variant<bar>>);
     static_assert(std::is_default_constructible_v<std::variant<int,bool,double,bar>>);
     static_assert(!std::is_default_constructible_v<std::variant<bar,int,bool,double>>);
+
+    static_assert(!std::is_nothrow_default_constructible_v<std::variant<bar>>);
+    static_assert(std::is_nothrow_default_constructible_v<std::variant<int,bool,double,bar>>);
+    static_assert(!std::is_nothrow_default_constructible_v<std::variant<bar,int,bool,double>>);
+
+    static_assert(std::is_nothrow_default_constructible_v<std::variant<foo,bar>>);
+    static_assert(!std::is_nothrow_default_constructible_v<std::variant<bar,foo>>);
+}
+
+void test_constructors()
+{
+    constexpr std::variant<foo,int,bool,double> v1;
+    static_assert(v1.index() == 0);
+    static_assert(std::holds_alternative<foo>(v1));
+
+    constexpr std::variant<int,foo,bar,bool,double> v2;
+    static_assert(v2.index() == 0);
+    static_assert(std::holds_alternative<int>(v2));
+}
+
+int main()
+{
+    test_type_traits();
+    test_constructors();
 }
