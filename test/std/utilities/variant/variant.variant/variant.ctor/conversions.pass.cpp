7bb56999cca63037d144056dea481b6b1bd30391
ruoka <ruoka@users.noreply.github.com>
2016-11-19 13:20:05 +0200

    more tests

--- test/std/utilities/variant/variant.variant/variant.ctor/conversions.pass.cpp
+++ test/std/utilities/variant/variant.variant/variant.ctor/conversions.pass.cpp
@@ -16,16 +16,29 @@ struct bar
     bar(bar&&) = delete;
 };
 
-int main()
+void test_type_traits()
 {
     // Well formed
     static_assert(std::is_nothrow_constructible_v<std::variant<foo>,foo>);
     static_assert(std::is_nothrow_constructible_v<std::variant<int,bool,double,foo>,int>);
     static_assert(std::is_nothrow_constructible_v<std::variant<foo,int,bool,double>,bool>);
+
     static_assert(!std::is_nothrow_constructible_v<std::variant<bar>,bar>);
     static_assert(std::is_nothrow_constructible_v<std::variant<int,bool,double,bar>,double>);
     static_assert(!std::is_nothrow_constructible_v<std::variant<bar,int,bool,double>,bar>);
+
+    static_assert(std::is_constructible_v<std::variant<std::string,foo>,std::string>);
+    static_assert(std::is_constructible_v<std::variant<std::string,foo>,const std::string&>);
+    static_assert(std::is_constructible_v<std::variant<std::string,foo>,std::string&&>);
+    static_assert(std::is_constructible_v<std::variant<std::string,foo>,char*>);
+    static_assert(std::is_constructible_v<std::variant<std::string,foo>,const char*>);
+
     // Ill formed
     static_assert(!std::is_nothrow_constructible_v<std::variant<bool,bool>,bool>);
     static_assert(!std::is_nothrow_constructible_v<std::variant<std::string,std::string>,const char*>);
+}
+
+int main()
+{
+    test_type_traits();
 }
