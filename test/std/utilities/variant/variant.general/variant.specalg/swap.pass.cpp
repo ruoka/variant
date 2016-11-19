cb1884c87deddba3704980309e44318b27fd9296
ruoka <ruoka@users.noreply.github.com>
2016-11-19 13:25:17 +0200

    fix

--- test/std/utilities/variant/variant.general/variant.specalg/swap.pass.cpp
+++ test/std/utilities/variant/variant.general/variant.specalg/swap.pass.cpp
@@ -8,9 +8,9 @@ int main()
     std::variant<float,unsigned,double,char> v1, v2;
     v1 = 123456u;
     v2 = 'A';
-    assert(std::get<1>(v1) = 123456u);
-    assert(std::get<3>(v2) = 'A');
+    assert(std::get<1>(v1) == 123456u);
+    assert(std::get<3>(v2) == 'A');
     std::swap<float,unsigned,double,char>(v1,v2);
-    assert(std::get<1>(v2) = 123456u);
-    assert(std::get<3>(v1) = 'A');
+    assert(std::get<1>(v2) == 123456u);
+    assert(std::get<3>(v1) == 'A');
 }
--- test/std/utilities/variant/variant.variant/variant.swap/swap.pass.cpp
+++ test/std/utilities/variant/variant.variant/variant.swap/swap.pass.cpp
@@ -8,9 +8,9 @@ int main()
     std::variant<float,unsigned,foo,char> v1, v2;
     v1 = 123456u;
     v2 = 'A';
-    assert(std::get<1>(v1) = 123456u);
-    assert(std::get<3>(v2) = 'A');
+    assert(std::get<1>(v1) == 123456u);
+    assert(std::get<3>(v2) == 'A');
     v1.swap(v2);
-    assert(std::get<1>(v2) = 123456u);
-    assert(std::get<3>(v1) = 'A');
+    assert(std::get<1>(v2) == 123456u);
+    assert(std::get<3>(v1) == 'A');
 }
