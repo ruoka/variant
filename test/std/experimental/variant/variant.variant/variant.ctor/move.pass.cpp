fc5ecabea2430a2bf8018a4387d0bd90a24ad775
ruoka <ruoka@users.noreply.github.com>
2016-11-20 02:37:30 +0200

    Fixes

--- test/std/experimental/variant/variant.variant/variant.ctor/move.pass.cpp
+++ test/std/experimental/variant/variant.variant/variant.ctor/move.pass.cpp
@@ -12,6 +12,8 @@
 #include <cassert>
 
 using std::experimental::variant;
+using std::experimental::holds_alternative;
+using std::experimental::variant_npos;
 
 struct foo
 {
@@ -23,7 +25,7 @@ struct bar
     bar(bar&&) = delete;
 };
 
-int main()
+void test_type_traits()
 {
     static_assert(std::is_move_constructible_v<variant<foo>>);
     static_assert(std::is_move_constructible_v<variant<int,bool,double,foo>>);
@@ -34,4 +36,23 @@ int main()
     static_assert(!std::is_move_constructible_v<variant<int,bool,double,bar>>);
     static_assert(!std::is_move_constructible_v<variant<bar,int,bool,double>>);
     static_assert(!std::is_move_constructible_v<variant<int,bool,bar,double>>);
+}
+
+void test_constructors()
+{
+    variant<int,bool,double> v1{2.2};
+    assert(v1.index() == 2);
+    assert(holds_alternative<double>(v1));
+
+    variant<int,bool,double> v2{std::move(v1)};
+    assert(v2.index() == 2);
+    assert(holds_alternative<double>(v2));
+
+    assert(v1.index() == variant_npos);
+}
+
+int main()
+{
+    test_type_traits();
+    test_constructors();
 }
