b8236b505e432cc5fbb9f0290688291e824414fb
ruoka <ruoka@users.noreply.github.com>
2017-08-04 10:25:13 +0300

    Fixed compilation issue with nullptr_t

--- test/std/utilities/variant/variant.general/variant.relops/less.pass.cpp
+++ test/std/utilities/variant/variant.general/variant.relops/less.pass.cpp
@@ -5,19 +5,18 @@
 
 using std::variant;
 
-void test_compile_time_equal_to()
+void test_compile_less()
 {
   using test = variant<bool,int,double,std::nullptr_t>;
   constexpr test t1{1};
-  constexpr test t2{1};
+  constexpr test t2{2};
   constexpr test t3{true};
-  static_assert(t1 == t2);
-  static_assert(!(t1 != t2));
-  static_assert(!(t1 == t3));
-  static_assert(t2 != t3);
+  static_assert(t1 < t2);
+  static_assert(!(t2 < t1));
+  static_assert(!(t1 < t3));
 }
 
 int main()
 {
-  test_compile_time_equal_to();
+  test_compile_less();
 }
